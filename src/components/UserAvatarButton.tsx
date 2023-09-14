87e05b986120af3fad6b4f7a24e94f81bf46a010
Rashid Aziz <aziz@sophire.com>
2023-09-14 16:03:00 -0700

    login styling

--- src/components/UserAvatarButton.tsx
+++ src/components/UserAvatarButton.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-
 //@ts-ignore
-import {useAuth } from "basictech-react"
-// import { useAuth } from "../lib.jsx";
+import { useAuth } from "basictech-react"
 
 function UserAvatarButton() {
   const { authState, login, logout } = useAuth();
@@ -25,7 +23,7 @@ function UserAvatarButton() {
   return (
     <div className="relative">
       <button
-        className="flex items-center justify-center w-8 h-8 rounded-full bg-gray-300 text-gray-700"
+        className="avatar rounded-full flex items-center justify-center  bg-base-300 text-gray-300"
         onClick={toggleDropdown}
       >
         {authState.isAuthenticated ? (
@@ -35,28 +33,40 @@ function UserAvatarButton() {
         )}
       </button>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+        <div className="absolute  menu right-0 mt-2 w-56 bg-base-300 rounded-md shadow-lg z-10">
           {authState.isAuthenticated ? (
-            <div className="p-4">
-              <p className="text-gray-700 font-medium">
+            <div className="p-4 text-left">
+              <p className="text-slate-100 font-medium">
                 {authState.user.name}
               </p>
-              <p className="text-gray-500">{authState.user.email}</p>
-              <button
-                className="mt-2 bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-md"
-                onClick={handleLogout}
-              >
-                Logout
-              </button>
+              <p className="text-gray-200">{authState.user.email}</p>
+              
+              <div className="divider"></div> 
+
+              <li onClick={handleLogout}><a className="text-grey-100">Logout</a></li>
+
             </div>
           ) : (
-            <div className="p-4">
+            <div className="p-2 text-left">
+              
+              <h6>You can always use tsk without creating an account. Your data will only be stored in your browser. </h6>
+
+              <h6>Login for more features:</h6>
+              <p> - sync between mobile & desktop</p>
+              <p> - share tasks with frens</p>
+              <p> - backup your tasks</p>
+
+              <div className="divider"></div> 
+
+
               <button
-                className="bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-md"
+                className="btn btn-md  w-full bg-gray-100 hover:bg-gray-200 text-gray-700 font-medium py-2 px-4 rounded-md"
                 onClick={handleLogin}
               >
-                Login
+                Login with Basic
               </button>
+              
+              <p className="pt-2 font-mono">tsk uses Basic for login, so your data is always private.</p>
             </div>
           )}
         </div>
