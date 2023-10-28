1f0f021bda197c5975e2b43ab7c22f6cdbc0574e
Rashid Aziz <aziz@sophire.com>
2023-10-27 17:52:09 -0700

    install PWA

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,20 @@ import "./index.css";
 //ts-ignore
 import {AuthProvider} from "basictech-react";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(
+      (registration) => {
+        console.log('SW registered: ', registration);
+      },
+      (registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      }
+    );
+  });
+}
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
