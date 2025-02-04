8fe89509e032c1f8c7399f65228663660d0e06ec
Rashid Aziz <aziz@sophire.com>
2025-02-03 22:53:47 -0800

    add status icon

--- src/App.tsx
+++ src/App.tsx
@@ -104,10 +104,44 @@ import bgImage from '/bg2.jpg';
 }
 
 
+function StatusIcon( {status}: {status: string}) {
+
+
+  return (
+    <div className="px-2 opacity-50">
+      {status === "OFFLINE" && (
+        <div className="text-red-500">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3" />
+        </svg>
+      </div>
+    )}
+    {status === "ONLINE" && (
+      <div className="text-green-500">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.111 16.404a5.5 5.5 0 017.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.14 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0" />
+        </svg>
+      </div>
+    )}
+    {status !== "OFFLINE" && status !== "ONLINE" && (
+      <div className="text-gray-500 animate-spin">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+        </svg>
+      </div>
+    )}
+    </div>
+  )
+}
+
+
+
 
 
 function Home() {
-  const { db } = useBasic();
+  const { db, dbStatus } = useBasic();
 
   const tasks = useQuery( () => db.collection("tasks").getAll())
   
@@ -185,10 +219,12 @@ function Home() {
           </form>
         </div>
 
-        <div className="flex-">
+        <div className="flex-none">
+          <StatusIcon status={dbStatus} />
           <UserAvatarButton />
         </div>
       </div>
+
 
       <div className="p-0 pb-24 md:pb-0">
         <div className="overflow-x-auto mt-10 flex justify-center">
