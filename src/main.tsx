202e7edc75ff76c16911202a42e31f8ae4db9e10
Rashid Aziz <aziz@sophire.com>
2025-04-16 12:29:15 -0700

    update basictech react

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './registerSW';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BasicProvider project_id={schema.project_id} schema={schema}>
+    <BasicProvider project_id={schema.project_id} schema={schema} debug>
       <App />
     </BasicProvider>
   </React.StrictMode>
